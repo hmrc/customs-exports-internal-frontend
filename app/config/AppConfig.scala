adfcb800fe0978aeb1c25c129664da155d21a4df
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-10-24 11:23:37 +0100

    CEDS 1825 arrive journey (#6)

---
+++ app/config/AppConfig.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import javax.inject.{Inject, Named, Singleton}
+import play.api.{Configuration, Environment, Mode}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+@Singleton
+class AppConfig @Inject()(
+  runModeConfiguration: Configuration,
+  servicesConfig: ServicesConfig,
+  environment: Environment,
+  @Named("appName") serviceIdentifier: String
+) {
+  private val contactBaseUrl = servicesConfig.baseUrl("contact-frontend")
+
+  private val assetsUrl = runModeConfiguration.get[String]("assets.url")
+
+  val runningAsDev: Boolean = {
+    runModeConfiguration
+      .getOptional[String]("run.mode")
+      .map(_.equals(Mode.Dev.toString))
+      .getOrElse(Mode.Dev.equals(environment.mode))
+  }
+  val assetsPrefix: String = assetsUrl + runModeConfiguration.get[String]("assets.version")
+  val analyticsToken: String = runModeConfiguration.get[String](s"google-analytics.token")
+  val analyticsHost: String = runModeConfiguration.get[String](s"google-analytics.host")
+  val reportAProblemPartialUrl: String = s"$contactBaseUrl/contact/problem_reports_ajax?service=$serviceIdentifier"
+  val reportAProblemNonJSUrl: String = s"$contactBaseUrl/contact/problem_reports_nonjs?service=$serviceIdentifier"
+  val authBaseUrl: String = servicesConfig.baseUrl("auth")
+
+  // from external UI
+
+  private def loadConfig(key: String): String =
+    runModeConfiguration
+      .getOptional[String](key)
+      .getOrElse(throw new Exception(s"Missing configuration key: $key"))
+
+  lazy val customsDeclarationsGoodsTakenOutOfEuUrl = loadConfig("urls.customsDeclarationsGoodsTakenOutOfEu")
+  lazy val serviceAvailabilityUrl = loadConfig("urls.serviceAvailability")
+}
---
+++ app/controllers/actions/AuthenticatedAction.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import config.AppConfig
+import connectors.StrideAuthConnector
+import controllers.exchanges.{AuthenticatedRequest, Operator}
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.Results.Forbidden
+import play.api.mvc._
+import play.api.{Configuration, Environment, Logger}
+import uk.gov.hmrc.auth.core.AuthProvider.PrivilegedApplication
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.{AuthProviders, AuthorisationException, AuthorisedFunctions, NoActiveSession}
+import views.html.unauthorized
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.HeaderCarrierConverter
+import uk.gov.hmrc.play.bootstrap.config.AuthRedirects
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class AuthenticatedAction @Inject()(
+  appConfig: AppConfig,
+  override val config: Configuration,
+  override val env: Environment,
+  override val authConnector: StrideAuthConnector,
+  mcc: MessagesControllerComponents,
+  unauthorizedPage: unauthorized
+) extends ActionBuilder[AuthenticatedRequest, AnyContent] with AuthorisedFunctions with AuthRedirects {
+
+  override implicit val executionContext: ExecutionContext = mcc.executionContext
+  override val parser: BodyParser[AnyContent] = mcc.parsers.defaultBodyParser
+
+  private val logger = Logger(classOf[AuthenticatedAction])
+
+  override def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]): Future[Result] = {
+    implicit val hc: HeaderCarrier =
+      HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
+    lazy val forbidden = Forbidden(unauthorizedPage()(request, mcc.messagesApi.preferred(request)))
+
+    authorised(AuthProviders(PrivilegedApplication)).retrieve(Retrievals.credentials) {
+      _.map { c =>
+        block(AuthenticatedRequest(Operator(c.providerId), request))
+      } getOrElse {
+        logger.debug("Missing credentials")
+        Future.successful(forbidden)
+      }
+    } recover {
+      case _: NoActiveSession =>
+        toStrideLogin(if (appConfig.runningAsDev) s"http://${request.host}${request.uri}" else request.uri)
+      case e: AuthorisationException =>
+        logger.debug("Authentication Failed", e)
+        forbidden
+    }
+  }
+}
