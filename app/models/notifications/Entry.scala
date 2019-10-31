e6f12fc91b6fc4450cd85ec4d065aac3e5eda75f
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-10-31 08:17:05 +0000

    CEDS-1860 Add NotificationFrontendModel

---
+++ app/models/notifications/Entry.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.notifications
+
+import models.UcrBlock
+import play.api.libs.json.Json
+
+case class Entry(ucrBlock: Option[UcrBlock] = None, goodsItem: Seq[GoodsItem] = Seq.empty, entryStatus: Option[EntryStatus] = None) {
+  def ucrType: Option[String] = ucrBlock.map(_.ucrType)
+  def ics: Option[String] = entryStatus.flatMap(_.ics)
+  def roe: Option[String] = entryStatus.flatMap(_.roe)
+  def soe: Option[String] = entryStatus.flatMap(_.soe)
+}
+
+object Entry {
+  implicit val format = Json.format[Entry]
+}
---
+++ app/models/notifications/NotificationFrontendModel.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.notifications
+
+import java.time.Instant
+
+import play.api.libs.json.Json
+
+case class NotificationFrontendModel(
+  timestampReceived: Instant = Instant.now(),
+  conversationId: String,
+  responseType: ResponseType,
+  entries: Seq[Entry],
+  crcCode: Option[String],
+  actionCode: Option[String],
+  errorCodes: Seq[String],
+  messageCode: String
+) extends Ordered[NotificationFrontendModel] {
+
+  override def compare(other: NotificationFrontendModel): Int =
+    this.timestampReceived.compareTo(other.timestampReceived)
+}
+
+object NotificationFrontendModel {
+  implicit val format = Json.format[NotificationFrontendModel]
+}
