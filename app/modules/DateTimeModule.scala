1a7f70e93b8c8d21bfa39bc013d87f81f7766093
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2019-11-06 09:40:37 +0000

    CEDS-1825 Remove modules from .gitignore and add missing module

---
+++ app/modules/DateTimeModule.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package modules
+
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+import com.google.inject.AbstractModule
+import javax.inject.{Inject, Provider, Singleton}
+
+class DateTimeModule extends AbstractModule {
+  def timezone = ZoneId.of("Europe/London")
+
+  override def configure(): Unit = {
+    bind(classOf[ZoneId]).toInstance(timezone)
+    bind(classOf[DateTimeFormatter]).toProvider(classOf[DateTimeFormatterProvider])
+  }
+}
+
+@Singleton
+class DateTimeFormatterProvider @Inject()(zoneId: ZoneId) extends Provider[DateTimeFormatter] {
+  override def get(): DateTimeFormatter =
+    DateTimeFormatter.ofPattern("dd MMM yyyy 'at' HH:mm").withZone(zoneId)
+}
