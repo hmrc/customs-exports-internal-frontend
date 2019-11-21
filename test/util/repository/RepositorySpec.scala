c22b43dfaca4f72d70e8d99ea18c5149593667da
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-11-21 16:24:13 +0000

    CEDS-PRA Configuration Refactor

--- test/util/repository/RepositorySpec.scala
+++
@@ -1,8 +0,0 @@
-package repository
-
-import org.scalatest.{MustMatchers, WordSpec}
-import uk.gov.hmrc.mongo.MongoSpecSupport
-
-abstract class RepositorySpec extends WordSpec with MustMatchers with MongoSpecSupport { self =>
-
-}
