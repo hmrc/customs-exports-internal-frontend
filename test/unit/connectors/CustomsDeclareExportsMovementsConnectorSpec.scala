6b2db6b1ef5e5a311e5394482e2ca35a2b7e5907
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-11-21 10:36:32 +0000

    CEDS-PRA Organise Tests into Unit & IT Source Sets

---
+++ test/unit/connectors/CustomsDeclareExportsMovementsConnectorSpec.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import config.AppConfig
+import connectors.exchanges.DisassociateDUCRRequest
+import forms.ConsignmentReferences
+import models.notifications.ResponseType.ControlResponse
+import models.requests.{MovementDetailsRequest, MovementRequest, MovementType}
+import org.mockito.BDDMockito._
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.http.Status
+import play.api.test.Helpers._
+import testdata.CommonTestData._
+import testdata.MovementsTestData.exampleSubmission
+import testdata.NotificationTestData.exampleNotificationFrontendModel
+
+class CustomsDeclareExportsMovementsConnectorSpec extends ConnectorSpec with MockitoSugar {
+
+  private val config = mock[AppConfig]
+  given(config.customsDeclareExportsMovementsUrl).willReturn(downstreamURL)
+
+  private val connector = new CustomsDeclareExportsMovementsConnector(config, httpClient)
+
+  "Submit Movement" should {
+
+    "POST to the Back End" in {
+      stubFor(
+        post("/movements")
+          .willReturn(
+            aResponse()
+              .withStatus(Status.ACCEPTED)
+          )
+      )
+
+      val request =
+        MovementRequest("eori", "provider-id", MovementType.Arrival, ConsignmentReferences("ref", "value"), MovementDetailsRequest("datetime"))
+      connector.submit(request).futureValue
+
+      verify(
+        postRequestedFor(urlEqualTo("/movements"))
+          .withRequestBody(
+            equalTo(
+              """{"eori":"eori","providerId":"provider-id","choice":"EAL","consignmentReference":{"reference":"ref","referenceValue":"value"},"movementDetails":{"dateTime":"datetime"}}"""
+            )
+          )
+      )
+    }
+  }
+
+  "Submit Consolidation" should {
+
+    "POST to the Back End" in {
+      stubFor(
+        post("/consolidation")
+          .willReturn(
+            aResponse()
+              .withStatus(Status.ACCEPTED)
+          )
+      )
+
+      val request = DisassociateDUCRRequest("provider-id", "eori", "ucr")
+      connector.submit(request).futureValue
+
+      verify(
+        postRequestedFor(urlEqualTo("/consolidation"))
+          .withRequestBody(equalTo("""{"providerId":"provider-id","eori":"eori","ucr":"ucr","consolidationType":"DISASSOCIATE_DUCR"}"""))
+      )
+    }
+  }
+
+  "fetch all Submissions" should {
+
+    "send GET request to the backend" in {
+
+      val expectedSubmission = exampleSubmission()
+      val submissionsJson =
+        s"""[
+           |  {
+           |    "uuid":"${expectedSubmission.uuid}",
+           |    "eori":"$validEori",
+           |    "conversationId":"$conversationId",
+           |    "ucrBlocks":[
+           |      {
+           |        "ucr":"$correctUcr",
+           |        "ucrType":"D"
+           |      }
+           |    ],
+           |    "actionType":"Arrival",
+           |    "requestTimestamp":"${expectedSubmission.requestTimestamp}"
+           |  }
+           |]""".stripMargin
+
+      stubFor(
+        get(s"/submissions?providerId=$providerId")
+          .willReturn(aResponse().withStatus(OK).withBody(submissionsJson))
+      )
+
+      val response = connector.fetchAllSubmissions(providerId).futureValue
+
+      val expectedUrl = s"/submissions?providerId=$providerId"
+      verify(getRequestedFor(urlEqualTo(expectedUrl)))
+
+      response mustBe Seq(expectedSubmission)
+    }
+  }
+
+  "fetch single Submission" should {
+
+    "send GET request to the backend" in {
+
+      val expectedSubmission = exampleSubmission()
+      val submissionJson =
+        s"""
+           |  {
+           |    "uuid":"${expectedSubmission.uuid}",
+           |    "eori":"$validEori",
+           |    "conversationId":"$conversationId",
+           |    "ucrBlocks":[
+           |      {
+           |        "ucr":"$correctUcr",
+           |        "ucrType":"D"
+           |      }
+           |    ],
+           |    "actionType":"Arrival",
+           |    "requestTimestamp":"${expectedSubmission.requestTimestamp}"
+           |  }
+           |""".stripMargin
+
+      stubFor(
+        get(s"/submissions/$conversationId?providerId=$providerId")
+          .willReturn(aResponse().withStatus(OK).withBody(submissionJson))
+      )
+
+      val response = connector.fetchSingleSubmission(conversationId, providerId).futureValue
+
+      val expectedUrl = s"/submissions/$conversationId?providerId=$providerId"
+      verify(getRequestedFor(urlEqualTo(expectedUrl)))
+
+      response mustBe Some(expectedSubmission)
+    }
+  }
+
+  "fetch Notifications" should {
+
+    "send GET request to the backend" in {
+
+      val expectedNotification = exampleNotificationFrontendModel()
+      val notificationsJson =
+        s"""[
+          |   {
+          |     "timestampReceived":"${expectedNotification.timestampReceived}",
+          |     "conversationId":"$conversationId",
+          |     "responseType":"${ControlResponse.value}",
+          |     "entries":[
+          |       {
+          |         "ucrBlock":{
+          |           "ucr":"$correctUcr",
+          |           "ucrType":"D"
+          |         },
+          |         "goodsItem":[]
+          |       }
+          |     ],
+          |     "errorCodes":[],
+          |     "messageCode":""
+          |   }
+          |]""".stripMargin
+
+      stubFor(
+        get(s"/notifications/$conversationId?providerId=$providerId")
+          .willReturn(aResponse().withStatus(OK).withBody(notificationsJson))
+      )
+
+      val response = connector.fetchNotifications(conversationId, providerId).futureValue
+
+      val expectedUrl = s"/notifications/$conversationId?providerId=$providerId"
+      verify(getRequestedFor(urlEqualTo(expectedUrl)))
+
+      response mustBe Seq(expectedNotification)
+    }
+  }
+}
---
+++ test/unit/controllers/movements/TransportControllerSpec.scala
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.movements
+
+import controllers.ControllerLayerSpec
+import forms.GoodsDeparted.DepartureLocation.OutOfTheUk
+import forms.providers.TransportFormProvider
+import forms.{GoodsDeparted, Transport}
+import models.cache.{Answers, ArrivalAnswers, Cache, DepartureAnswers}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, verify, when}
+import play.api.data.Form
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import play.twirl.api.HtmlFormat
+import services.MockCache
+import testdata.CommonTestData.providerId
+import uk.gov.hmrc.play.bootstrap.tools.Stubs.stubMessagesControllerComponents
+import views.html.transport
+
+import scala.concurrent.ExecutionContext.global
+
+class TransportControllerSpec extends ControllerLayerSpec with MockCache {
+
+  private val formProvider = mock[TransportFormProvider]
+  private val page = mock[transport]
+
+  private def controller(answers: Answers = DepartureAnswers()) =
+    new TransportController(SuccessfulAuth(), ValidJourney(answers), cache, formProvider, stubMessagesControllerComponents(), page)(global)
+
+  override protected def beforeEach(): Unit = {
+    super.beforeEach()
+
+    when(formProvider.provideForm(any())).thenReturn(Transport.outOfTheUkForm)
+    when(page.apply(any())(any(), any())).thenReturn(HtmlFormat.empty)
+  }
+
+  override protected def afterEach(): Unit = {
+    reset(formProvider)
+    reset(page)
+
+    super.afterEach()
+  }
+
+  private def theResponseForm: Form[Transport] = {
+    val captor = ArgumentCaptor.forClass(classOf[Form[Transport]])
+    verify(page).apply(captor.capture())(any(), any())
+    captor.getValue
+  }
+
+  private def answersPassedToFormProvider: DepartureAnswers = {
+    val captor = ArgumentCaptor.forClass(classOf[DepartureAnswers])
+    verify(formProvider).provideForm(captor.capture())
+    captor.getValue
+  }
+
+  "Location Controller on GET" should {
+
+    "return 200 (OK)" when {
+
+      "invoked without data for Transport in cache" in {
+
+        val answers = DepartureAnswers(goodsDeparted = Some(GoodsDeparted(OutOfTheUk)))
+        givenTheCacheContains(Cache(providerId, answers))
+
+        val result = controller(answers).displayPage()(getRequest)
+
+        status(result) mustBe OK
+        theResponseForm.value mustBe empty
+      }
+
+      "invoked with data for Transport in cache" in {
+
+        val cachedGoodsDeparted = Some(GoodsDeparted(OutOfTheUk))
+        val cachedTransport = Some(Transport(Some("1"), Some("GB"), Some("123")))
+        val answers = DepartureAnswers(goodsDeparted = cachedGoodsDeparted, transport = cachedTransport)
+        givenTheCacheContains(Cache(providerId, answers))
+
+        val result = controller(answers).displayPage()(getRequest)
+
+        status(result) mustBe OK
+        theResponseForm.value mustBe cachedTransport
+      }
+    }
+
+    "return 303 (SEE_OTHER)" when {
+
+      "there is no goods departed in the cache" in {
+
+        givenTheCacheIsEmpty()
+
+        val result = controller(DepartureAnswers()).displayPage()(getRequest)
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).get mustBe routes.GoodsDepartedController.displayPage().url
+      }
+    }
+  }
+
+  "Location Controller on POST" when {
+
+    "provided with incorrect form" should {
+
+      "return 400 (BAD_REQUEST)" in {
+
+        val answers = DepartureAnswers(goodsDeparted = Some(GoodsDeparted(OutOfTheUk)))
+        givenTheCacheContains(Cache(providerId, answers))
+
+        val invalidForm = Json.toJson(Transport(Some("99"), Some("Invalid"), Some("Invalid")))
+
+        val result = controller(answers).saveTransport()(postRequest(invalidForm))
+
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "user is on a different journey" should {
+
+      "return 403 (FORBIDDEN)" in {
+
+        givenTheCacheIsEmpty()
+
+        val correctForm = Json.toJson(Transport(Some("1"), Some("GB"), Some("123")))
+
+        val result = controller(ArrivalAnswers()).saveTransport()(postRequest(correctForm))
+
+        status(result) mustBe FORBIDDEN
+      }
+    }
+
+    "provided with correct form for departure" should {
+
+      "call FormProvider passing answers with GoodsDeparted element" in {
+
+        val answers = DepartureAnswers(goodsDeparted = Some(GoodsDeparted(OutOfTheUk)))
+        givenTheCacheContains(Cache(providerId, answers))
+
+        val correctForm = Json.toJson(Transport(Some("1"), Some("GB"), Some("123")))
+
+        await(controller(answers).saveTransport()(postRequest(correctForm)))
+
+        answersPassedToFormProvider mustBe answers
+      }
+
+      "return 303 (SEE_OTHER)" in {
+
+        val answers = DepartureAnswers(goodsDeparted = Some(GoodsDeparted(OutOfTheUk)))
+        givenTheCacheContains(Cache(providerId, answers))
+
+        val correctForm = Json.toJson(Transport(Some("1"), Some("GB"), Some("123")))
+
+        val result = controller(answers).saveTransport()(postRequest(correctForm))
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(controllers.movements.routes.MovementSummaryController.displayPage().url)
+      }
+    }
+  }
+
+}
