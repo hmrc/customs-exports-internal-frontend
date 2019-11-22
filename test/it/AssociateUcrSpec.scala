cb908b9d9af19960d021911ec91b02ef5403fb66
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-11-22 11:14:03 +0000

    CEDS-PRA Consolidation Integration Tests (#65)
    * CEDS-PRA Add test for each journey on ChoiceSpec
    
    * CEDS-PRA Shut Mucr Intergration Spec
    
    * CEDS-PRA Associate UCR Integration Spec
    
    * CEDS-PRA Scalafmt
    
    * CEDS-PRA Dissociate UCR Integration Test
    
    * CEDS-PRA Scalafmt

---
+++ test/it/AssociateUcrSpec.scala
@@ -0,0 +1,112 @@
+import com.github.tomakehurst.wiremock.client.WireMock.{equalTo, verify}
+import forms.MucrOptions.CreateOrAddValues.Create
+import forms.{AssociateKind, AssociateUcr, MucrOptions}
+import models.cache.AssociateUcrAnswers
+import play.api.test.Helpers._
+
+class AssociateUcrSpec extends IntegrationSpec {
+
+  "UCR Options Page" when {
+    "GET" should {
+      "return 200" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", AssociateUcrAnswers())
+
+        val response = get(controllers.consolidations.routes.MucrOptionsController.displayPage())
+
+        status(response) mustBe OK
+      }
+    }
+
+    "POST" should {
+      "continue" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", AssociateUcrAnswers())
+
+        val response = post(controllers.consolidations.routes.MucrOptionsController.submit(), "createOrAdd" -> "create", "newMucr" -> "GB/123-12345")
+
+        status(response) mustBe SEE_OTHER
+        redirectLocation(response) mustBe Some(controllers.consolidations.routes.AssociateUCRController.displayPage().url)
+        theCacheFor("pid") mustBe Some(AssociateUcrAnswers(mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345"))))
+      }
+    }
+  }
+
+  "Associate UCR Page" when {
+    "GET" should {
+      "return 200" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", AssociateUcrAnswers(mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345"))))
+
+        val response = get(controllers.consolidations.routes.AssociateUCRController.displayPage())
+
+        status(response) mustBe OK
+      }
+    }
+
+    "POST" should {
+      "continue" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", AssociateUcrAnswers(mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345"))))
+
+        val response = post(controllers.consolidations.routes.AssociateUCRController.submit(), "kind" -> "mucr", "mucr" -> "GB/321-54321")
+
+        status(response) mustBe SEE_OTHER
+        redirectLocation(response) mustBe Some(controllers.consolidations.routes.AssociateUCRSummaryController.displayPage().url)
+        theCacheFor("pid") mustBe Some(
+          AssociateUcrAnswers(
+            mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345")),
+            associateUcr = Some(AssociateUcr(AssociateKind.Mucr, "GB/321-54321"))
+          )
+        )
+      }
+    }
+  }
+
+  "Associate UCR Summary Page" when {
+    "GET" should {
+      "return 200" in {
+        givenAuthSuccess("pid")
+        givenCacheFor(
+          "pid",
+          AssociateUcrAnswers(
+            mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345")),
+            associateUcr = Some(AssociateUcr(AssociateKind.Mucr, "GB/321-54321"))
+          )
+        )
+
+        val response = get(controllers.consolidations.routes.AssociateUCRSummaryController.displayPage())
+
+        status(response) mustBe OK
+      }
+    }
+
+    "POST" should {
+      "continue" in {
+        givenAuthSuccess("pid")
+        givenCacheFor(
+          "pid",
+          AssociateUcrAnswers(
+            mucrOptions = Some(MucrOptions(createOrAdd = Create, newMucr = "GB/123-12345")),
+            associateUcr = Some(AssociateUcr(AssociateKind.Mucr, "GB/321-54321"))
+          )
+        )
+        givenMovementsBackendAcceptsTheConsolidation()
+
+        val response = post(controllers.consolidations.routes.AssociateUCRSummaryController.submit())
+
+        status(response) mustBe SEE_OTHER
+        redirectLocation(response) mustBe Some(controllers.consolidations.routes.AssociateUCRConfirmationController.display().url)
+        theCacheFor("pid") mustBe None
+        verify(
+          postRequestedForConsolidation()
+            .withRequestBody(
+              equalTo(
+                """{"providerId":"pid","eori":"GB1234567890","mucr":"GB/123-12345","ucr":"GB/321-54321","consolidationType":"ASSOCIATE_DUCR"}"""
+              )
+            )
+        )
+      }
+    }
+  }
+}
---
+++ test/it/DissociateUcrSpec.scala
@@ -0,0 +1,72 @@
+import com.github.tomakehurst.wiremock.client.WireMock.{equalTo, verify}
+import forms.{DisassociateKind, DisassociateUcr}
+import models.cache.DisassociateUcrAnswers
+import play.api.test.Helpers._
+
+class DissociateUcrSpec extends IntegrationSpec {
+
+  "Dissociate UCR Page" when {
+    "GET" should {
+      "return 200" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", DisassociateUcrAnswers())
+
+        val response = get(controllers.consolidations.routes.DisassociateUCRController.display())
+
+        status(response) mustBe OK
+      }
+    }
+
+    "POST" should {
+      "continue" in {
+        givenAuthSuccess("pid")
+        givenCacheFor("pid", DisassociateUcrAnswers())
+
+        val response = post(controllers.consolidations.routes.DisassociateUCRController.submit(), "kind" -> "mucr", "mucr" -> "GB/321-54321")
+
+        status(response) mustBe SEE_OTHER
+        redirectLocation(response) mustBe Some(controllers.consolidations.routes.DisassociateUCRSummaryController.display().url)
+        theCacheFor("pid") mustBe Some(
+          DisassociateUcrAnswers(ucr = Some(DisassociateUcr(kind = DisassociateKind.Mucr, mucr = Some("GB/321-54321"), ducr = None)))
+        )
+      }
+    }
+  }
+
+  "Dissociate UCR Summary Page" when {
+    "GET" should {
+      "return 200" in {
+        givenAuthSuccess("pid")
+        givenCacheFor(
+          "pid",
+          DisassociateUcrAnswers(ucr = Some(DisassociateUcr(kind = DisassociateKind.Mucr, mucr = Some("GB/321-54321"), ducr = None)))
+        )
+
+        val response = get(controllers.consolidations.routes.DisassociateUCRSummaryController.display())
+
+        status(response) mustBe OK
+      }
+    }
+
+    "POST" should {
+      "continue" in {
+        givenAuthSuccess("pid")
+        givenCacheFor(
+          "pid",
+          DisassociateUcrAnswers(ucr = Some(DisassociateUcr(kind = DisassociateKind.Mucr, mucr = Some("GB/321-54321"), ducr = None)))
+        )
+        givenMovementsBackendAcceptsTheConsolidation()
+
+        val response = post(controllers.consolidations.routes.DisassociateUCRSummaryController.submit())
+
+        status(response) mustBe SEE_OTHER
+        redirectLocation(response) mustBe Some(controllers.consolidations.routes.DisassociateUCRConfirmationController.display().url)
+        theCacheFor("pid") mustBe None
+        verify(
+          postRequestedForConsolidation()
+            .withRequestBody(equalTo("""{"providerId":"pid","eori":"GB1234567890","ucr":"GB/321-54321","consolidationType":"DISASSOCIATE_DUCR"}"""))
+        )
+      }
+    }
+  }
+}
