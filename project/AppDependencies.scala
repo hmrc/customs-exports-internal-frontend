308b5c08dc12b5e2afa06809ba686ead702eaed5
Mauricio Fernandes de Castro <mauricio.de.castro@digital.hmrc.gov.uk>
2020-01-23 11:45:36 +0000

    CEDS-2098 - Update Final Confirmation Screen Testing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo"          % "7.22.0-play-26",
-    "uk.gov.hmrc" %% "govuk-template"                % "5.48.0-play-26",
-    "uk.gov.hmrc" %% "play-json-union-formatter"     % "1.7.0",
-    "uk.gov.hmrc" %% "play-ui"                       % "8.5.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-play-26"             % "1.3.0",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.2.0-play-26",
+    "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.2.0-play-26",
+    "uk.gov.hmrc"          %% "logback-json-logger"           % "4.6.0",
+    "uk.gov.hmrc"          %% "govuk-template"                % "5.48.0-play-26",
+    "uk.gov.hmrc"          %% "play-health"                   % "3.14.0-play-26",
+    "uk.gov.hmrc"          %% "play-ui"                       % "8.5.0-play-26",
+    "uk.gov.hmrc"          %% "bootstrap-play-26"             % "1.3.0",
+    "uk.gov.hmrc"          %% "play-frontend-govuk"           % "0.32.0-play-26",
+    "org.webjars.npm"      %  "govuk-frontend"                % "3.4.0",
+    "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.7.0",
+    "uk.gov.hmrc"          %% "simple-reactivemongo"          % "7.22.0-play-26",
+    "ai.x"                 %% "play-json-extensions"          % "0.40.2",
     "com.github.tototoshi" %% "scala-csv" % "1.3.6"
   ).map(_.withSources())
 
