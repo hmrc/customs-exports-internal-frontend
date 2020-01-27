123f311530dcb52786deb3d577aa3af6cdf5a17e
Maurício Fernandes de Castro <992887+iservport@users.noreply.github.com>
2020-01-27 14:31:29 +0000

    CEDS-2098 - Associate UCR page (#94)
    This is a feature branch to consolidate merges from all related page-specific branches:
    
    * CEDS-2098-arrive (affects departure and retrospective arrival) - ok
    * CEDS-2098-associate - ok
    * CEDS-2098-disassociate - TODO
    * CEDS-2098-shut - TODO
    * CEDS-2098-clean - TODO
    
    It is an alternative confirmation screen to implement in the internal service as a way of avoiding cases being closed when they shouldn't be.
    
    Currently, NCH operators see the existing confirmation screen at the end of each movement request journey and believe that it means their request has been completed and accepted. As their request has only been submitted to DMS and the acceptance (or rejection) is asynchronous, this could be misleading.

---
+++ test/unit/views/associate_ucr/AssociateUCRConfirmationViewSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.associate_ucr
+
+import base.Injector
+import play.api.test.FakeRequest
+import views.ViewSpec
+import views.html.associate_ucr_confirmation
+
+class AssociateUCRConfirmationViewSpec extends ViewSpec with Injector {
+
+  private implicit val request = FakeRequest()
+  private val page = instanceOf[associate_ucr_confirmation]
+
+  "AssociateUCRConfirmationView" when {
+
+    "View is rendered" should {
+
+      "render title" in {
+
+        page().getTitle must containMessage("movement.confirmation.title.ASSOCIATE_UCR")
+      }
+
+      "render header" in {
+
+        page()
+          .getElementsByClass("govuk-heading-xl")
+          .first() must containMessage("movement.confirmation.title.ASSOCIATE_UCR")
+      }
+
+      "have 'notification timeline' link" in {
+        val inset = page().getElementsByClass("govuk-inset-text").first()
+        inset
+          .getElementsByClass("govuk-link")
+          .first() must haveHref(controllers.routes.ViewSubmissionsController.displayPage())
+      }
+
+      "have 'find another consignment' link" in {
+        page()
+          .getElementsByClass("govuk-link")
+          .get(1) must haveHref(controllers.routes.ChoiceController.displayPage())
+      }
+    }
+  }
+
+}
