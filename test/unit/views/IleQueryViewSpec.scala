30c5719477839a2eb2fe353d95fcf405997cd1e1
Mauricio Fernandes de Castro <mauricio.de.castro@digital.hmrc.gov.uk>
2020-02-07 15:07:19 +0000

    CEDS-2100 - Make searching for a consignment the first step in the service
    The "Find a consignment" page must be first step in the internal movements service, so that NCH operators always search for a consignment before performing an action on it.

--- test/unit/views/IleQueryViewSpec.scala
+++ test/unit/views/IleQueryViewSpec.scala
@@ -16,16 +16,20 @@
 
 package views
 
+import base.Injector
 import forms.IleQueryForm
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import play.api.mvc.{AnyContent, Request}
 import play.api.test.FakeRequest
+import play.twirl.api.Html
 import views.html.ile_query
 
-class IleQueryViewSpec extends ViewSpec {
+class IleQueryViewSpec extends ViewSpec with Injector {
 
   private implicit val request: Request[AnyContent] = FakeRequest().withCSRFToken
 
-  private val page = new ile_query(main_template)
+  private val page = instanceOf[ile_query]
   private val view = page(IleQueryForm.form)
 
   "Ile Query page" should {
@@ -37,24 +41,43 @@ class IleQueryViewSpec extends ViewSpec {
 
     "render page header" in {
 
-      view.getElementById("title").text() mustBe messages("ileQuery.title")
+      view.getElementsByClass("govuk-fieldset__heading").first().text() mustBe messages("ileQuery.title")
     }
 
     "render error summary" when {
 
       "no errors" in {
 
-        view.getErrorSummary mustBe empty
+        val govukErrorSummary = view.getElementsByClass("govuk-error-summary__title").first()
+
+        Option(govukErrorSummary) mustBe None
       }
 
       "some errors" in {
-        page(IleQueryForm.form.withError("error", "error.required")).getErrorSummary mustBe defined
+
+        val errorView = page(IleQueryForm.form.withError("error", "error.required"))
+
+        val govukErrorSummary = errorView.getElementsByClass("govuk-error-summary__title").first()
+
+        govukErrorSummary.text() mustBe messages("error.summary.title")
       }
     }
 
+    val govukListElement = view.getElementsByClass("govuk-list").first()
+
     "contains submit button" in {
 
-      view.getElementById("submit").text() mustBe messages("site.continue")
+      view.getElementsByClass("govuk-button").first().text() mustBe messages("site.continue")
+    }
+
+    "contains link to create empty mucr" in {
+
+      govukListElement.getElementsByClass("govuk-link").get(0).text() mustBe messages("ileQuery.link.emptyMucr")
+    }
+
+    "contains link to view previous requests" in {
+
+      govukListElement.getElementsByClass("govuk-link").get(1).text() mustBe messages("ileQuery.link.requests")
     }
   }
 }
