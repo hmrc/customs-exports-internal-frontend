bd81911e69304b86f3c7a23d63edbadbf696f1fc
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-02-14 11:11:38 +0000

    CEDS-2110 - tidy up code following review (#129)
    * CEDS-2110 - tidy up code following review
    
    * CEDS-2110 - renamed IleQueryCodeConverter; simplified transport conv.

--- app/models/viewmodels/decoder/SOECode.scala
+++ app/models/viewmodels/decoder/SOECode.scala
@@ -50,7 +50,7 @@ object SOECode {
 
   val MucrCodes: Set[SOECode] = Set(ConsolidationOpen, ConsolidationClosedWithoutP2P, ConsolidationHasP2P)
 
-  val CombinedCodes: Set[SOECode] = MucrCodes ++ DucrCodes
+  val AllCodes: Set[SOECode] = MucrCodes ++ DucrCodes
 
   case object DeclarationValidation extends SOECode(code = "1", messageKey = "decoder.soe.DeclarationValidation")
 
