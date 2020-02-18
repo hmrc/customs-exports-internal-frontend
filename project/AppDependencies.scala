8c0a6b037827d0e9ec5935ee1cac81ea65f1105b
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-02-18 12:24:15 +0000

    BAU - update mongo-java-driver to latest version (#143)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,8 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "simple-reactivemongo"          % "7.22.0-play-26",
     "ai.x"                 %% "play-json-extensions"          % "0.40.2",
     "com.github.tototoshi" %% "scala-csv"                     % "1.3.6",
-    "com.github.cloudyrock.mongock"  %  "mongock-core"                      % "2.0.2"
+    "com.github.cloudyrock.mongock"  %  "mongock-core"                      % "2.0.2",
+    "org.mongodb"          %  "mongo-java-driver"             % "3.12.1"
   ).map(_.withSources())
 
   val test = Seq(
