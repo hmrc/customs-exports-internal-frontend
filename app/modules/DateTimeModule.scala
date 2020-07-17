d9a21c42ebe86abb8c8c2cf0d3ba894875ff2cd1
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2020-07-17 13:31:10 +0100

    CEDS-2634 Remove DateTimeFormatterProvider

--- app/modules/DateTimeModule.scala
+++ app/modules/DateTimeModule.scala
@@ -16,27 +16,18 @@
 
 package modules
 
-import java.time.format.DateTimeFormatter
 import java.time.{Clock, ZoneId}
 
 import com.google.inject.AbstractModule
-import javax.inject.{Inject, Provider, Singleton}
 import modules.DateTimeModule.timezone
 
 class DateTimeModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[ZoneId]).toInstance(timezone)
-    bind(classOf[DateTimeFormatter]).toProvider(classOf[DateTimeFormatterProvider])
     bind(classOf[Clock]).toInstance(Clock.system(timezone))
   }
 }
 
 object DateTimeModule {
   lazy val timezone: ZoneId = ZoneId.of("Europe/London")
-}
-
-@Singleton
-class DateTimeFormatterProvider @Inject()(zoneId: ZoneId) extends Provider[DateTimeFormatter] {
-  override def get(): DateTimeFormatter =
-    DateTimeFormatter.ofPattern("dd MMM yyyy 'at' HH:mm").withZone(zoneId)
 }
