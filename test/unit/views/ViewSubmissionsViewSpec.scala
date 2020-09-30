21d1bb86345e00e96a963447eea4b29bfdf4a879
janeturner <jane.turner@digital.hmrc.gov.uk>
2020-09-30 13:11:06 +0100

    CEDS-2689 Add parameters to mkString and test case for multiple UCRs

--- test/unit/views/ViewSubmissionsViewSpec.scala
+++ test/unit/views/ViewSubmissionsViewSpec.scala
@@ -132,13 +132,38 @@ class ViewSubmissionsViewSpec extends ViewSpec with Injector {
         )
       )
 
+      val associateSubmission = Submission(
+        requestTimestamp = dateTime.plus(30, MINUTES),
+        eori = "",
+        conversationId = conversationId_4,
+        ucrBlocks = Seq(UcrBlock(ucr = validDucr, ucrType = "D"), UcrBlock(ucr = validMucr, ucrType = "M")),
+        actionType = ConsolidationType.DucrAssociation
+      )
+      val associateNotifications = Seq(
+        exampleNotificationFrontendModel(
+          timestampReceived = dateTime.plus(34, MINUTES),
+          conversationId = conversationId_4,
+          responseType = ResponseType.ControlResponse,
+          entries = Seq(
+            Entry(ucrBlock = Some(UcrBlock(ucr = validDucr, ucrType = "D"))),
+            Entry(ucrBlock = Some(UcrBlock(ucr = validMucr, ucrType = "M")))
+          )
+        )
+      )
+
       val pageWithData: Document = createView(
-        Seq(shutMucrSubmission -> shutMucrNotifications, arrivalSubmission -> arrivalNotifications, departureSubmission -> departureNotifications)
+        Seq(
+          shutMucrSubmission -> shutMucrNotifications,
+          arrivalSubmission -> arrivalNotifications,
+          departureSubmission -> departureNotifications,
+          associateSubmission -> associateNotifications
+        )
       )
 
       val firstDataRowElements = pageWithData.selectFirst(".govuk-table__body .govuk-table__row:nth-child(1)")
       val secondDataRowElements = pageWithData.selectFirst(".govuk-table__body .govuk-table__row:nth-child(2)")
       val thirdDataRowElements = pageWithData.selectFirst(".govuk-table__body .govuk-table__row:nth-child(3)")
+      val fourthDataRowElements = pageWithData.selectFirst(".govuk-table__body .govuk-table__row:nth-child(4)")
 
       firstDataRowElements.selectFirst(".ucr").text() mustBe s"$validMucr ${messages("submissions.hidden.text", validMucr)}"
       firstDataRowElements.selectFirst(".submission-type").text() mustBe "MUCR"
@@ -154,6 +179,11 @@ class ViewSubmissionsViewSpec extends ViewSpec with Injector {
       thirdDataRowElements.selectFirst(".submission-type") must containMessage("submissions.submissionType.DP")
       thirdDataRowElements.selectFirst(".date-of-request").text() mustBe viewDates.formatDateAtTime(LocalDateTime.of(2019, 10, 31, 0, 33))
       thirdDataRowElements.selectFirst(".submission-action") must containMessage("submissions.departure")
+
+      fourthDataRowElements.selectFirst(".ucr").text() mustBe s"$validDucr $validMucr ${messages("submissions.hidden.text", validDucr + ", " + validMucr)}"
+      fourthDataRowElements.selectFirst(".submission-type") must containMessage("submissions.submissionType.D")
+      fourthDataRowElements.selectFirst(".date-of-request").text() mustBe viewDates.formatDateAtTime(LocalDateTime.of(2019, 10, 31, 0, 30))
+      fourthDataRowElements.selectFirst(".submission-action") must containMessage("submissions.ducrassociation")
     }
 
     "contain MUCR and DUCR if Submission contains both" in {
