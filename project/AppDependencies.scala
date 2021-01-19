55d5ab5ae2f8e64ebc8fa4c30a71ba429bd3c65c
janeturner <jane.turner@digital.hmrc.gov.uk>
2021-01-19 09:38:11 +0000

    CEDS-2939 Upgrade scalatest to v3.2.3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -28,7 +28,7 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"                    % "1.13.1"                % "test, it",
     "com.typesafe.play"       %% "play-test"                % current                 % "test, it",
     "org.mockito"             %  "mockito-core"             % "3.5.7"                 % "test, it",
-    "org.scalatestplus"       %% "mockito-3-4"              % "3.2.3.0"               % "test",
+    "org.scalatestplus"       %% "mockito-3-4"              % "3.2.3.0"               % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"                % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.27.1"                % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
