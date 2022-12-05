7ea2d8dc304ea3c8cace11021297c030267141c3
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2022-12-05 12:24:24 +0000

    CEDS-4251 Update version of play and play-fronted-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % "5.24.0",
     "uk.gov.hmrc"          %% "logback-json-logger"           % "5.2.0",
     "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.21.0-play-28",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.33.0-play-28",
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"            % "0.68.0",
     "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.16.0-play-28",
     "ai.x"                 %% "play-json-extensions"          % "0.42.0",
