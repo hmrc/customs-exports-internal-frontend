b1602a2659365c7a75ef5dc54fadeb80bd214340
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2022-12-20 14:34:30 +0000

    CEDS-4258 Update dependencies and configuration

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % "5.24.0",
-    "uk.gov.hmrc"          %% "logback-json-logger"           % "5.2.0",
     "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.12.0-play-28",
     "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.33.0-play-28",
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"            % "0.68.0",
