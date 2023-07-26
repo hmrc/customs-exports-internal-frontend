f92feaba0d07a7315fe6a7fe1e46e88d7669aa9a
Mohammad Dweik <89045034+Dweik97@users.noreply.github.com>
2023-07-26 12:18:58 +0100

    CEDS-4739: mongodb-driver upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
 
   val bootstrapPlayVersion = "7.20.0"
-  val hmrcMongoVersion = "0.74.0"
+  val hmrcMongoVersion = "1.3.0"
   val jacksonVersion = "2.14.2"
 
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28"    % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
     // Used by the Migration tool. Keep this library's version to the same major.minor version as the mongo-scala-driver.
-    "org.mongodb" % "mongodb-driver-sync" % "4.6.1",
+    "org.mongodb" % "mongodb-driver-sync" % "4.8.2",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"                   %% "play-frontend-hmrc"            % "7.15.0-play-28",
     "uk.gov.hmrc"                   %% "play-json-union-formatter"     % "1.18.0-play-28",
