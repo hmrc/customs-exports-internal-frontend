95bb948ff1383ce2386d0c3b29aa31b717ce44f1
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-12-05 16:34:21 +0000

    CEDS-6202 Added more tests for FindCdsConsignmentController

--- test/controllers/FindCdsConsignmentControllerSpec.scala
+++ test/controllers/FindCdsConsignmentControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import forms.FindCdsUcr
@@ -14,12 +30,8 @@ import scala.concurrent.ExecutionContext.global
 class FindCdsConsignmentControllerSpec extends ControllerLayerSpec with MockIleQueryCache with MockCache with ScalaFutures with IntegrationPatience {
   private val findCdsConsignmentPage = mock[find_cds_consignment]
 
-  private def controller = new FindCdsConsignmentController(
-    stubMessagesControllerComponents(),
-    SuccessfulAuth(),
-    cacheRepository,
-    findCdsConsignmentPage,
-  )(global)
+  private def controller =
+    new FindCdsConsignmentController(stubMessagesControllerComponents(), SuccessfulAuth(), cacheRepository, findCdsConsignmentPage)(global)
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
@@ -40,16 +52,21 @@ class FindCdsConsignmentControllerSpec extends ControllerLayerSpec with MockIleQ
     }
   }
 
-  "provided with semantically incorrect UCR" should {
-    val incorrectUCR = "123ABC-789456POIUYT"
+  "Calling submitCdsConsignment" should {
 
-    "return Find CDS Consignment page, passing form with errors" in {
+    "return BAD_REQUEST when passing form with errors" in {
+      val incorrectUCR = "123ABC-789456POIUYT"
+      val result = controller.submitCdsConsignment(postRequest(Json.obj("ucr" -> incorrectUCR)))
+      status(result) mustBe BAD_REQUEST
+    }
 
-      controller.submitCdsConsignment(getRequest)
+    "return SEE_OTHER when passing form without errors" in {
 
-      val expectedForm = FindCdsUcr.form.fillAndValidate(FindCdsUcr(incorrectUCR))
-      verify(findCdsConsignmentPage).apply(meq(expectedForm))(any(), any())
+      val correctUCR = "GB/123456789100-AB123"
+      val result = controller.submitCdsConsignment(postRequest(Json.obj("ucr" -> correctUCR)))
+      status(result) mustBe SEE_OTHER
     }
   }
+
 
 }
