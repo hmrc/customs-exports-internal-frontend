5f284a3248ef98fe0960c22a28271c3b0ab40128
tomndennis <tomndennis@users.noreply.github.com>
2025-12-10 15:14:23 +0000

    DCWL-2996 - Dependency upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object Dependencies {
 
   val bootstrapPlayVersion = "9.14.0"
-  val frontendPlayVersion = "12.8.0"
-  val hmrcMongoVersion = "2.7.0"
+  val frontendPlayVersion = "12.22.0"
+  val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapPlayVersion,
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % frontendPlayVersion,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"           %% "play-json-union-formatter"             % "1.22.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.webjars.npm"       %  "accessible-autocomplete"               % "3.0.0",
