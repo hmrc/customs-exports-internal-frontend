2c5da5fe73539269a960560b41553980c6c39c09
tomndennis <tomndennis@users.noreply.github.com>
2026-08-18 12:24:14 +0100

    DCWL-4945 - fix tests with adding new govuk links

--- test/views/summary/AssociateUcrSummaryViewSpec.scala
+++ test/views/summary/AssociateUcrSummaryViewSpec.scala
@@ -62,7 +62,7 @@ class AssociateUcrSummaryViewSpec extends ViewSpec with Injector {
     "render change link" when {
 
       def validateChangeLink(view: Html, call: Call): Unit = {
-        val changeUcr = view.getElementsByClass("govuk-link").first()
+        val changeUcr = view.getElementsByClass("govuk-link").get(1)
         changeUcr must containMessage("site.change")
         changeUcr must haveHref(call)
       }
